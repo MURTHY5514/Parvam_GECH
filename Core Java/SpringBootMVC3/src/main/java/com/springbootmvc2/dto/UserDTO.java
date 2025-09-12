71fb2fc3916588ed51df1102b9c32e39ba16629e
MURTHY <87651957+MURTHY5514@users.noreply.github.com>
2025-09-12 14:40:18 +0530

    updated code

--- Core Java/SpringBootMVC3/src/main/java/com/springbootmvc2/dto/UserDTO.java
+++ Core Java/SpringBootMVC3/src/main/java/com/springbootmvc2/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.springbootmvc2.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 
 public class UserDTO {
